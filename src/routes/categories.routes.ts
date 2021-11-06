36245be71439062f879650cbf4913274d86964fe
jairodias <jairopereira_dias@hotmail.com>
2021-11-06 19:45:42 -0300

    criando classes para a responsibilidade unica e implementando a iunversao de dependencia

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,18 +9,14 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadExists) {
-    return response.status(400).json({ error: "Category already exists." });
-  }
-
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
 
-categoriesRoutes.get("/", (request, response) => {
+categoriesRoutes.get("/", (_, response) => {
   const all = categoriesRepository.list();
 
   return response.status(200).json(all);
