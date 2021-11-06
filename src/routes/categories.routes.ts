842770695db6fbddf2c25af37c4919b53c2de3a5
jairodias <jairopereira_dias@hotmail.com>
2021-11-06 19:23:56 -0300

    criando repositorio com DTO - Data Transfer Object, sendo utilizado com interfaces

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,16 @@
 import { Router } from "express";
-import { v4 as uuid } from "uuid";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuid(),
-    name,
-    description,
-  };
+  categoriesRepository.create({ name, description });
 
-  categories.push(category);
-
-  return response.status(201);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
