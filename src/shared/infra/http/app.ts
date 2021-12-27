bbde96663b9036f2abd1f86e68ab31bad54a148b
jairodias <jairopereira_dias@hotmail.com>
2021-12-27 18:48:05 -0300

    adicionado o super test e criando teste de integraçao com da criaçao de categoria

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+
+import { AppError } from "@shared/errors/AppError";
+import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
+
+import swaagerFile from "../../../swagger.json";
+
+import "@shared/container";
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaagerFile));
+
+app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${error.message}`,
+    });
+  }
+);
+
+export { app };
