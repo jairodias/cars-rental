8900dae91b2b203d123957e038a03a46c999c2cd
jairodias <jairopereira_dias@hotmail.com>
2021-12-29 10:33:42 -0300

    adicionado o envio de email de recuperaçao de senha

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,47 @@
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { inject, injectable } from "tsyringe";
+import { v4 as uuid } from "uuid";
+
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private readonly usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private readonly usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private readonly dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private readonly mailProvider: IMailProvider
+  ) {}
+
+  async execute(email: string): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("User does not exists.");
+    }
+
+    const token = uuid();
+    const expires_date = this.dateProvider.addHours(3);
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date,
+    });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      `O link para o reset é ${token}`
+    );
+  }
+}
+
+export { SendForgotPasswordMailUseCase };
