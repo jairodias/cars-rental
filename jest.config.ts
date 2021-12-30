e43e7f0d7cd730502182fa162792a519a40c1635
jairodias <jairopereira_dias@hotmail.com>
2021-12-30 07:51:24 -0300

    criando o coverage

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,10 @@ export default {
   coverageProvider: "v8",
   preset: "ts-jest",
   testMatch: ["**/*.spec.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text-summary", "lcov"],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src/",
   }),
