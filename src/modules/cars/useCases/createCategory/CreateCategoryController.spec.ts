8ff252ff604b28f7542f276087433c9dc416b23b
jairodias <jairopereira_dias@hotmail.com>
2021-12-30 07:59:30 -0300

    ajuste de refresh_token nos testes

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -35,13 +35,13 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
       .send({ name: "valid_name", description: "valid_description" })
       .set({
-        Authorization: `Bearer ${refresh_token}`,
+        Authorization: `Bearer ${token}`,
       });
 
     expect(response.status).toBe(201);
@@ -53,13 +53,13 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
       .send({ name: "valid_name", description: "valid_description" })
       .set({
-        Authorization: `Bearer ${refresh_token}`,
+        Authorization: `Bearer ${token}`,
       });
 
     expect(response.status).toBe(400);
--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -35,13 +35,13 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     await request(app)
       .post("/categories")
       .send({ name: "valid_name", description: "valid_description" })
       .set({
-        Authorization: `Bearer ${refresh_token}`,
+        Authorization: `Bearer ${token}`,
       });
 
     const response = await request(app).get("/categories");
